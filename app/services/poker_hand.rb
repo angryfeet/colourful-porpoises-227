421e7ae76198c14759366acd4eb7e0d2e140d34a
Lyndsey Gardiner <lyndsey@lyndseygardiner.co.uk>
2023-01-15 18:53:05 +0000

    Add validations to the service

--- app/services/poker_hand.rb
+++ app/services/poker_hand.rb
@@ -4,5 +4,41 @@ class PokerHand
 
   attr_accessor :card_input
 
+  validates_presence_of :card_input
 
+  validate :valid_hand_size
+  def valid_hand_size
+    errors.add(:base, "is more than five cards") if cards.length > 5
+    errors.add(:base, "is less than five cards") if cards.length < 5
+  end
+
+  validate :valid_cards
+  def valid_cards
+    invalid_suits = suits.select { |s| !["H", "D", "S", "C"].include?(s) }
+    errors.add(:base, "contains unknown suits") if invalid_suits.present?
+    invalid_faces = faces.select { |f| !(["A", "J", "Q", "K"] + (2..10).to_a).include?(f) }
+    errors.add(:base, "contains unknown faces") if invalid_faces.present?
+  end
+
+  validate :duplicate_cards
+  def duplicate_cards
+    errors.add(:base, 'contains duplicate cards') unless cards.uniq.length == 5
+    errors.add(:base, "can't have five cards of the same type") if faces.uniq.length == 1
+  end
+
+  def card_input
+    @card_input ||= []
+  end
+
+  def cards
+    @card_input&.split(' ')
+  end
+
+  def suits
+    cards.map(&:last)
+  end
+
+  def faces
+    cards.map { |x| x[0..-2]}
+  end
 end
