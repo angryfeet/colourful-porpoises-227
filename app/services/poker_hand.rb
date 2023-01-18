bae555fcae7ca81f0b57ec63c1a6a9c45d016f43
Lyndsey Gardiner <lyndsey@lyndseygardiner.co.uk>
2023-01-18 16:02:03 +0000

    Fill in hand methods to pass tests

--- app/services/poker_hand.rb
+++ app/services/poker_hand.rb
@@ -31,28 +31,45 @@ class PokerHand
   end
 
   def straight_flush?
+    flush? && straight?
   end
 
   def four_of_a_kind?
+    face_groups.any? { |_, x| x.length == 4 }
   end
 
   def full_house?
+    face_groups.length == 2 &&
+      face_groups.any? { |_, x| x.length == 2 } &&
+      face_groups.any? { |_, x| x.length == 3 }
   end
 
   def flush?
+    suits.uniq.length == 1
   end
 
   def straight?
+    straight = face_values.sort.each_cons(2).all? { |x, y| x + 1 == y }
+    straight = face_values(ace_high: false).sort.each_cons(2).all? { |x, y| x + 1 == y } if !straight && faces.any? { |x| x == 'A' }
+    straight
   end
 
   def three_of_a_kind?
+    face_groups.any? { |_, x| x.length == 3 } &&
+      face_groups.length == 3
   end
 
   def two_pair?
+    face_groups.select { |_, x| x.length == 2 }.length == 2
   end
 
   def one_pair?
+    face_groups.select { |_, x| x.length == 2 }.length == 1 &&
+      face_groups.length == 4
   end
+
+  private
+
   def cards
     @card_input&.split(' ')
   end
@@ -62,6 +79,27 @@ class PokerHand
   end
 
   def faces
-    cards.map { |x| x[0..-2]}
+    cards.map { |x| x[0..-2] }
+  end
+
+  def face_values(ace_high: true)
+    faces.map do |f|
+      case f
+      when 'K'
+        '13'
+      when 'Q'
+        '12'
+      when 'J'
+        '11'
+      when 'A'
+        ace_high ? '14' : '1'
+      else
+        f
+      end
+    end.map(&:to_i)
+  end
+
+  def face_groups
+    faces.group_by(&:itself)
   end
 end
